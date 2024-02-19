b6947de9864ecf80745ee997c3db1d5544f00af8
User <Willspri@gmail.com>
2024-02-19 16:05:14 +1300

    added break to repeat function

--- Atbash/Atbash.py
+++ Atbash/Atbash.py
@@ -1,19 +1,23 @@
+while True:
+    plaintext = input("Enter Message to encrypt or decrypt: ").upper()
 
-plaintext = input("Enter Message to encrypt or decrypt").upper()
+    ciphertext = ""
+    for letter in plaintext:
+        ascii = ord(letter)
+        # keep between A and Z ascii
+        if ascii >= 65 and ascii <= 90:
+            # subtract 65 from ascii representation of letter
+            position = ascii - 65
+            # apply Atbash formula
+            NewPos = 25 - position
+            newAscii = NewPos + 65
+            # chr returns an ascii value as character
+            NewLetter = chr(newAscii)
+        else:
+            NewLetter = letter
+        ciphertext = ciphertext + NewLetter
+    print("Encrypted/Decrypted Message:", ciphertext)
 
-ciphertext = ""
-for letter in plaintext:
-    ascii = ord(letter)
-    #keep between A and Z ascii
-    if ascii>=65 and ascii<=90:
-    #subtract 65 from ascii representation of letter
-        position = ascii - 65
-        #apply Atbash formula 
-        NewPos = 25 - position 
-        newAscii = NewPos + 65
-        #chr returns an ascii value as character
-        NewLetter =chr(newAscii)
-    else:
-        NewLetter = letter
-    ciphertext = ciphertext + NewLetter
-print(ciphertext)
+    repeat = input("Do you want to encrypt or decrypt another message? (yes/no): ").lower()
+    if repeat != 'yes':
+        break
