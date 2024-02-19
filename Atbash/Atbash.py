32cd5a836abd7767ca37b456c03158a775ccbe97
User <Willspri@gmail.com>
2024-02-19 15:36:08 +1300

    testing commands

--- Atbash/Atbash.py
+++ Atbash/Atbash.py
@@ -7,4 +7,13 @@ ascii = ord(letter)
 print(ascii)
 #subtract 65 from ascii representation of letter
 position = ascii - 65
-print(position)
+#print(position)
+
+#apply Atbash formula 
+NewPos = 25 - position 
+#print(NewPos)
+
+newAscii = NewPos + 65
+#chr returns an ascii value as character
+NewLetter =chr(newAscii)
+print(NewLetter)
