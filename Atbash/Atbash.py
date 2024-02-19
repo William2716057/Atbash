24ff0348558d9a1b0af096938323b5b733ff658a
User <Willspri@gmail.com>
2024-02-19 15:57:03 +1300

    code cleaned

--- Atbash/Atbash.py
+++ Atbash/Atbash.py
@@ -2,24 +2,21 @@
 #encrypt one letter at a time
 #letter = "A"
 
-plaintext = "message".upper()
+plaintext = input("Enter Message to encrypt or decrypt").upper()
+
+ciphertext = ""
 for letter in plaintext:
     ascii = ord(letter)
     #keep between A and Z ascii
     if ascii>=65 and ascii<=90:
-    #print(ascii)
     #subtract 65 from ascii representation of letter
         position = ascii - 65
-        #print(position)
-
         #apply Atbash formula 
         NewPos = 25 - position 
-        #print(NewPos)
-
         newAscii = NewPos + 65
         #chr returns an ascii value as character
         NewLetter =chr(newAscii)
-        #print(NewLetter)
     else:
         NewLetter = letter
-    print(NewLetter)
+    ciphertext = ciphertext + NewLetter
+print(ciphertext)
