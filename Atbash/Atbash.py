c1f3505422e8c97bc1a4a2fb2570df1a5d55880b
User <Willspri@gmail.com>
2024-02-19 15:45:41 +1300

    remove spaces

--- Atbash/Atbash.py
+++ Atbash/Atbash.py
@@ -2,19 +2,24 @@
 #encrypt one letter at a time
 #letter = "A"
 
-plaintext = "message"
+plaintext = "message".upper()
 for letter in plaintext:
     ascii = ord(letter)
+    #keep between A and Z ascii
+    if ascii>=65 and ascii<=90:
     #print(ascii)
     #subtract 65 from ascii representation of letter
-    position = ascii - 65
-    #print(position)
+        position = ascii - 65
+        #print(position)
 
-    #apply Atbash formula 
-    NewPos = 25 - position 
-    #print(NewPos)
+        #apply Atbash formula 
+        NewPos = 25 - position 
+        #print(NewPos)
 
-    newAscii = NewPos + 65
-    #chr returns an ascii value as character
-    NewLetter =chr(newAscii)
+        newAscii = NewPos + 65
+        #chr returns an ascii value as character
+        NewLetter =chr(newAscii)
+        #print(NewLetter)
+    else:
+        NewLetter = letter
     print(NewLetter)
