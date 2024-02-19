3bab69dc8110d3aa4d8fc713f30afbba6f023ab6
User <Willspri@gmail.com>
2024-02-19 15:38:37 +1300

    turned into loop

--- Atbash/Atbash.py
+++ Atbash/Atbash.py
@@ -1,19 +1,20 @@
 
 #encrypt one letter at a time
-letter = "A"
+#letter = "A"
 
-ascii = ord(letter)
+plaintext = "message"
+for letter in plaintext:
+    ascii = ord(letter)
+    #print(ascii)
+    #subtract 65 from ascii representation of letter
+    position = ascii - 65
+    #print(position)
 
-print(ascii)
-#subtract 65 from ascii representation of letter
-position = ascii - 65
-#print(position)
+    #apply Atbash formula 
+    NewPos = 25 - position 
+    #print(NewPos)
 
-#apply Atbash formula 
-NewPos = 25 - position 
-#print(NewPos)
-
-newAscii = NewPos + 65
-#chr returns an ascii value as character
-NewLetter =chr(newAscii)
-print(NewLetter)
+    newAscii = NewPos + 65
+    #chr returns an ascii value as character
+    NewLetter =chr(newAscii)
+    print(NewLetter)
